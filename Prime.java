ee3d120cce39bc14e6f90b417ab2e0ef9555aaa2
Rahul Kongara <rahul.kongara13@gmail.com>
2024-12-11 11:29:52 +0530

    5th Commit are --

--- Prime.java
+++ Prime.java
@@ -1,6 +1,6 @@
 public class Prime {
     public static void main(String[] args) {
-        System.out.println("Prime numbers from 1 to 100 are:");
+        System.out.println("Prime numbers from 1 to 100:");
         for (int i = 2; i <= 100; i++) {
             if (isItPrime(i)) {
                 // System.out.print(i + " ");
