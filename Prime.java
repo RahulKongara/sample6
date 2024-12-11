42ba59c2b25da2e894a3181cf4f39f41f997d60b
Rahul Kongara <rahul.kongara13@gmail.com>
2024-12-11 11:29:14 +0530

    4th Commit isPrime -> isItPrime

--- Prime.java
+++ Prime.java
@@ -2,19 +2,19 @@ public class Prime {
     public static void main(String[] args) {
         System.out.println("Prime numbers from 1 to 100 are:");
         for (int i = 2; i <= 100; i++) {
-            if (isPrime(i)) {
+            if (isItPrime(i)) {
                 // System.out.print(i + " ");
                 System.out.print(i + '\t');
             }
         }
     }
 
-    private static boolean isPrime(int n) {
+    private static boolean isItPrime(int n) {
         if (n <= 1) return false;
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) return false;
         }
         return true;
     }
-    
+
 }
